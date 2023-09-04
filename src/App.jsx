8646839b7ebcd97cba88b666a772bfb6236b726f
shouhei <shohei.nagamura@gmail.com>
2023-09-05 03:08:18 +0900

    タスク表示にテェックボックスを表示

--- src/App.jsx
+++ src/App.jsx
@@ -96,6 +96,7 @@ function App() {
             ? "Loading..."
             : tasks.map((task) => (
                 <li key={task.id}>
+                  <input type="checkbox" />
                   {task.id}:{task.task}
                   <button onClick={() => updateTask(task.id)}>更新</button>
                   <button onClick={() => deleteTask(task.id)}>削除</button>
